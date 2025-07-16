da17ce68878254525d4dc89d1eaf486d6095408a
Pratyush Kumar Swain <pratyushkumarswain101@gmail.com>
2025-07-16 21:02:05 +0530

    Routes configured

--- src/components/Routes/AppRoutes.jsx
+++ src/components/Routes/AppRoutes.jsx
@@ -1,6 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainHeader from "../MainHeader/MainHeader";
 import Home from "../Home/Home";
+import About from '../About/About';
+import Skills from '../Skills/Skills';
+import Projects from '../Projects/Projects';
+import Contact from '../Contact/Contact';
+import Error from '../Error/Error';
 
 const AppRoutes = () => {
 
@@ -9,7 +14,12 @@ const AppRoutes = () => {
             <Routes>
                 <Route path='/' element={ <MainHeader /> }>
                     <Route index element={ <Home /> } />
+                    <Route path='/about' element={ <About /> } />
+                    <Route path='/skills' element={ <Skills /> } />
+                    <Route path='/projects' element={ <Projects /> } />
+                    <Route path='/contact' element={ <Contact /> } />
                 </Route>
+                <Route path='*' element={ <Error /> } />
             </Routes>
         </BrowserRouter>
     );
