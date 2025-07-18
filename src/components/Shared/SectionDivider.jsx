cfa44013797266a1d76624f3a9d6b6cb5dcd7a46
Pratyush Kumar Swain <pratyushkumarswain101@gmail.com>
2025-07-18 10:06:18 +0530

    Added and Implemented SectionDivider Section

---
+++ src/components/Shared/SectionDivider.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './SectionDivider.css';
+
+const SectionDivider = () => {
+    return <div className="section-divider" data-aos="fade-in"></div>;
+};
+
+export default SectionDivider;
