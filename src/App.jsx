e49a1a665d61c8e5c825e6d0022fb3140375b196
Pratyush Kumar Swain <pratyushkumarswain101@gmail.com>
2025-07-18 10:16:48 +0530

    AOS and ScrollToTop implementd in App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import AppRoutes from './components/Routes/AppRoutes';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 
 function App() {
 
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true,
+      offset: 80,
+      easing: 'ease-in-out',
+    });
+  }, []);
+
   return (
-    <AppRoutes />
+    <>
+      <AppRoutes />
+      <ScrollToTop />
+    </>
   );
 }
 
