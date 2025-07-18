f3a1e952b8f6e140fc7fcea48e06b1ac51c6becd
Pratyush Kumar Swain <pratyushkumarswain101@gmail.com>
2025-07-18 10:11:11 +0530

    Implemented Complete Home Page UI

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,29 @@
+import AboutPreview from "../AboutPreview/AboutPreview";
+import CTASection from "../CTASection/CTASection";
+import Footer from "../Footer/Footer";
+import GitHubStats from "../GitHubStats/GitHubStats";
 import Hero from "../Hero/Hero";
+import Highlights from "../Highlights/Highlights";
+import SectionDivider from "../Shared/SectionDivider";
+import TechStack from "../TechStack/TechStack";
 
 const Home = () => {
-    
+
     return (
-        <Hero />
+        <>
+            <Hero />
+            <SectionDivider />
+            <AboutPreview />
+            <SectionDivider />
+            <Highlights />
+            <SectionDivider />
+            <TechStack />
+            <SectionDivider />
+            <GitHubStats />
+            <SectionDivider />
+            <CTASection />
+            <Footer />
+        </>
     );
 }
 
