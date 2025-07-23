65742827ac89d5f7d61ae482cb1e6fe94fb7280a
Pratyush Kumar Swain <pratyushkumarswain101@gmail.com>
2025-07-23 23:32:32 +0530

    Minor bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import AppRoutes from './components/Routes/AppRoutes';
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,18 @@
+import { useEffect } from 'react';
+import Hero from "../Hero/Hero";
+import SectionDivider from "../Shared/SectionDivider";
 import AboutPreview from "../AboutPreview/AboutPreview";
+import Highlights from "../Highlights/Highlights";
+import TechStack from "../TechStack/TechStack";
+import GitHubStats from "../GitHubStats/GitHubStats";
 import CTASection from "../CTASection/CTASection";
 import Footer from "../Footer/Footer";
-import GitHubStats from "../GitHubStats/GitHubStats";
-import Hero from "../Hero/Hero";
-import Highlights from "../Highlights/Highlights";
-import SectionDivider from "../Shared/SectionDivider";
-import TechStack from "../TechStack/TechStack";
 
 const Home = () => {
+
+    useEffect(() => {
+        document.title = 'Pratyush Swain | MERN Fullstack Developer';
+    }, [])
 
     return (
         <>
