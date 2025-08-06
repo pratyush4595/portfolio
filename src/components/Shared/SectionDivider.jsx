98398b191c6af5ef0e509ccecc73e71228c39524
Pratyush Kumar Swain <pratyushkumarswain101@gmail.com>
2025-08-06 17:00:45 +0530

    src removed from git, due to copy right issue

--- src/components/Shared/SectionDivider.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './SectionDivider.css';
-
-const SectionDivider = () => {
-    return <div className="section-divider" data-aos="fade-in"></div>;
-};
-
-export default SectionDivider;
